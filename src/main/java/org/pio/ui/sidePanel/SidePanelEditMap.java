fc76bef780b813a3a911cbdafe00d18463d8e47d
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2023-10-31 21:29:21 +0100

    Add initButton method, draw button in hashmap

--- src/main/java/org/pio/ui/sidePanel/SidePanelEditMap.java
+++ src/main/java/org/pio/ui/sidePanel/SidePanelEditMap.java
@@ -1,23 +1,38 @@
 package org.pio.ui.sidePanel;
 
-import org.pio.ui.buttons.aButton;
+import org.pio.ui.buttons.AbstractMyButton;
+import org.pio.ui.buttons.MyButton;
 
 import java.awt.*;
 import java.util.HashMap;
 
 public class SidePanelEditMap extends aSidePanel{
 
-    private HashMap<Integer, aButton> buttons = new HashMap<>();
+    private HashMap<Integer, AbstractMyButton> buttons = new HashMap<>();
 
     public SidePanelEditMap(int width, int height, int posX, int posY) {
         super(width, height, posX, posY);
 
-        //buttons.put(1, )
+        initButtons();
+    }
+
+    private void initButtons() {
+        int mapId = 1;
+
+        int buttonPosX = posX;
+        int buttonPosY = posY;
+        int buttonWidth = 100;
+        int buttonHeight = 100;
+        int buttonId = 1;
+
+        buttons.put(mapId++, MyButton.createButtonWithText("BUTTON_1", buttonPosX, buttonPosY, buttonWidth, buttonHeight, buttonId, "NO_TEXT"));
     }
 
     @Override
     public void draw(Graphics g) {
         super.draw(g);
+
+        buttons.forEach((k, v) -> v.draw(g));
     }
 
 }
