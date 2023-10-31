b8f3e8081ec9fe50da6a2780e2cc106c721cbd17
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2023-10-31 20:49:44 +0100

    Add constructors to MyButton; prepare to delete old button classes

--- src/main/java/org/pio/ui/buttons/MyButton.java
+++ src/main/java/org/pio/ui/buttons/MyButton.java
@@ -1,4 +1,38 @@
 package org.pio.ui.buttons;
 
+import java.awt.image.BufferedImage;
+
 public class MyButton extends AbstractMyButton{
+    private MyButton(String name, int posX, int posY, int width, int height, int id) {
+        super(name, posX, posY, width, height, id);
+    }
+
+    private MyButton(String name, int posX, int posY, int width, int height, int id, String text) {
+        super(name, posX, posY, width, height, id, text);
+    }
+
+    private MyButton(String name, int posX, int posY, int width, int height, int id, BufferedImage image) {
+        super(name, posX, posY, width, height, id, image);
+    }
+
+    private MyButton(String name, int posX, int posY, int width, int height, int id, String text, BufferedImage image) {
+        super(name, posX, posY, width, height, id, text, image);
+    }
+
+    public static MyButton createButtonWithoutTextAndImage(String name, int posX, int posY, int width, int height, int id){
+        return new MyButton(name, posX, posY, width, height, id);
+    }
+
+    public static MyButton createButtonWithText(String name, int posX, int posY, int width, int height, int id, String text){
+        return new MyButton(name, posX, posY, width, height, id, text);
+    }
+
+    public static MyButton createButtonWithImage(String name, int posX, int posY, int width, int height, int id, BufferedImage image){
+        return new MyButton(name, posX, posY, width, height, id, image);
+    }
+
+    public static MyButton createButtonWithTextAndImage(String name, int posX, int posY, int width, int height, int id, String text, BufferedImage image){
+        return new MyButton(name, posX, posY, width, height, id, text, image);
+    }
+
 }
