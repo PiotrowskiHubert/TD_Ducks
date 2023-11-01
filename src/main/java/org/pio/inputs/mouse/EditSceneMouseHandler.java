6223c3f506cd4370c75b02ee34c543e2cf872f54
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2023-11-01 20:49:57 +0100

    Prepare EditSceneMouseHandler

--- src/main/java/org/pio/inputs/mouse/EditSceneMouseHandler.java
+++ src/main/java/org/pio/inputs/mouse/EditSceneMouseHandler.java
@@ -1,4 +1,36 @@
 package org.pio.inputs.mouse;
 
-public class EditSceneMouseHandler {
+import org.pio.scene.EditScene;
+
+public class EditSceneMouseHandler implements MouseHandler{
+    EditScene editScene;
+
+    public EditSceneMouseHandler(EditScene editScene) {
+        this.editScene = editScene;
+    }
+
+    @Override
+    public void leftMouseClicked(int x, int y) {
+
+    }
+
+    @Override
+    public void rightMouseClicked(int x, int y) {
+
+    }
+
+    @Override
+    public void mouseMoved(int x, int y) {
+
+    }
+
+    @Override
+    public void mousePressed(int x, int y) {
+
+    }
+
+    @Override
+    public void mouseReleased(int x, int y) {
+
+    }
 }
