207b8302f9348795e49f68dc0fb304c36bbb4e28
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2023-12-01 19:26:47 +0100

    Add EnemyDraw class

--- src/main/java/org/pio/entities/enemy/Enemy.java
+++ src/main/java/org/pio/entities/enemy/Enemy.java
@@ -1,6 +1,6 @@
 package org.pio.entities.enemy;
 
-import org.pio.entities.Entity;
+import org.pio.entities.entity.Entity;
 import org.pio.helpz.KeyPoint;
 import org.pio.helpz.Directions;
 
@@ -11,13 +11,14 @@ import java.util.LinkedList;
 import java.util.Objects;
 import java.util.Stack;
 
-public abstract class Enemy extends Entity {
+public abstract class Enemy extends Entity  {
     public double movementSpeed, updatesPerSec;
     public int health, damage, gold, keypointIndex, currentSprite=0, maxSprite=7;
     public Directions direction;
     public LinkedHashMap<Directions, LinkedList<BufferedImage>> sprites;
     public Stack<KeyPoint> keyPointsStack;
     public EnemyUpdate enemyUpdate;
+    public EnemyDraw enemyDraw;
 
     public Enemy(String name,  int id, int health, int damage, int gold, double movementSpeed, int width, int height, LinkedHashMap<Directions, LinkedList<BufferedImage>> sprites) {
         super(name, id, width, height);
@@ -39,31 +40,16 @@ public abstract class Enemy extends Entity {
 
         this.direction = direction;
 
-        this.keyPointsStack=new Stack<>();
-        this.keypointIndex=1;
+        this.keyPointsStack = new Stack<>();
+        this.keypointIndex = 1;
         this.keyPointsStack.push(startKeyPoint);
-
+        this.enemyDraw = new EnemyDraw(this);
         //this.enemyUpdate=new EnemyUpdate(this);
     }
 
     @Override
     public void draw(Graphics g) {
-        switch (direction) {
-            case LEFT -> {
-                g.drawImage(sprites.get(direction).get(currentSprite), bounds.getBounds().x - 38, bounds.getBounds().y - 96, null);
-            }
-            case RIGHT -> {
-                g.drawImage(sprites.get(direction).get(currentSprite), bounds.getBounds().x - 38, bounds.getBounds().y - 96, null);
-            }
-            case UP -> {
-                g.drawImage(sprites.get(direction).get(currentSprite), bounds.getBounds().x - 38, bounds.getBounds().y - 96, null);
-            }
-            case DOWN -> {
-                g.drawImage(sprites.get(direction).get(currentSprite), bounds.getBounds().x - 38, bounds.getBounds().y - 96,   null);
-            }
-        }
-        g.setColor(Color.BLACK);
-        g.drawRect(bounds.getBounds().x, bounds.getBounds().y, bounds.getBounds().width, bounds.getBounds().height);
+      enemyDraw.draw(g);
     }
 
     @Override
@@ -73,7 +59,6 @@ public abstract class Enemy extends Entity {
         Enemy enemy = (Enemy) o;
         return Double.compare(enemy.movementSpeed, movementSpeed) == 0 && health == enemy.health && damage == enemy.damage && gold == enemy.gold && keypointIndex == enemy.keypointIndex && currentSprite == enemy.currentSprite && maxSprite == enemy.maxSprite && direction == enemy.direction && Objects.equals(sprites, enemy.sprites) && Objects.equals(keyPointsStack, enemy.keyPointsStack) && Objects.equals(enemyUpdate, enemy.enemyUpdate);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(movementSpeed, health, damage, gold, direction, sprites, keyPointsStack, keypointIndex, currentSprite, maxSprite, enemyUpdate);
