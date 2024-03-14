66259077a60cf158e4a76770e1156c82aa356d07
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-03-14 22:41:24 +0100

    init test class

---
+++ src/test/java/TxtReadersTest.java
@@ -0,0 +1,59 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.pio.sprites.SpriteDetails;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class TxtReadersTest {
+
+    @Test
+    public void testGetModifiedIntegerFromTxtFileWithKey(){
+        Path path = Path.of("src/main/resources/AllyInfo/sprites/blue/character_blue_idle_49x72.txt");
+
+        int numOfSpritesBefore = 0;
+        int extraModifier = 0;
+        String key = "UP_1_WIDTH:";
+
+        Assert.assertEquals(18, (int) SpriteDetails.getModifiedIntegerFromTxtFileWithKey(
+                path,
+                key,
+                (SpriteDetails.getModifierIntegerFromTxtFileForSubImages(
+                        path,
+                        numOfSpritesBefore
+                    )+extraModifier)
+                )
+        );
+
+        numOfSpritesBefore = 1;
+        extraModifier = 0;
+        key = "UP_2_WIDTH:";
+
+        Assert.assertEquals(19, (int) SpriteDetails.getModifiedIntegerFromTxtFileWithKey(
+                        path,
+                        key,
+                        (SpriteDetails.getModifierIntegerFromTxtFileForSubImages(
+                                path,
+                                numOfSpritesBefore
+                        )+extraModifier)
+                )
+        );
+    }
+
+
+
+    @Test
+    public void testReadModifierFromTxtFile(){
+        Path path = Path.of("src/main/resources/AllyInfo/sprites/blue/character_blue_idle_49x72.txt");
+
+        int numOfSpritesBefore = 0;
+        Assert.assertEquals(0, (int) SpriteDetails.getModifierIntegerFromTxtFileForSubImages(path, numOfSpritesBefore));
+
+        numOfSpritesBefore = 1;
+        Assert.assertEquals(1, (int) SpriteDetails.getModifierIntegerFromTxtFileForSubImages(path, numOfSpritesBefore));
+
+        numOfSpritesBefore = 2;
+        Assert.assertEquals(1, (int) SpriteDetails.getModifierIntegerFromTxtFileForSubImages(path, numOfSpritesBefore));
+
+    }
+}
