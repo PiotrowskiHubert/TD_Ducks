c6c45c091fccc77f1829c76950a1929f9a7f0755
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2024-03-14 22:44:44 +0100

    add new sprite to spriteAtlasDatabase

--- src/main/java/org/pio/database/MainDatabase.java
+++ src/main/java/org/pio/database/MainDatabase.java
@@ -1,9 +1,11 @@
 package org.pio.database;
 
+import lombok.Data;
 import org.pio.entities.ally.*;
 import org.pio.entities.enemy.*;
 import org.pio.helpz.ReadFromFileImpl;
 import org.pio.helpz.Directions;
+import org.pio.sprites.SpriteDetails;
 import org.pio.tiles.Tile;
 import org.pio.tiles.aTile;
 
@@ -11,12 +13,15 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 
+@Data
 public class MainDatabase {
     public LinkedHashMap<Integer, Enemy> enemyDatabase = new LinkedHashMap<>();
     public LinkedHashMap<Integer, Ally> allyDatabase = new LinkedHashMap<>();
+    public LinkedHashMap<String, SpriteDetails> allySpriteDetails = new LinkedHashMap<>();
     public LinkedHashMap<String, BufferedImage> spriteAtlasDatabase = new LinkedHashMap<>();
     public LinkedHashMap<Integer, aTile> grassTileSet;
 
@@ -28,6 +33,7 @@ public class MainDatabase {
         spriteAtlasDatabase.put("SidePanel",getSpriteAtlas("SidePanel.png"));
         spriteAtlasDatabase.put("Buttons",getSpriteAtlas("AtlasButtons.png"));
         spriteAtlasDatabase.put("GrassTileSet", setSpriteAtlas("Sprite/GrassTileSet.png"));
+        spriteAtlasDatabase.put("Character_blue_idle_49x112", setSpriteAtlas("AllyInfo/sprites/blue/character_blue_idle_49x112.png"));
 
         grassTileSet=setGrassTileSet();
 
@@ -37,11 +43,11 @@ public class MainDatabase {
         enemySpriteAtlasDatabase.put(4, getEnemySpriteAtlas_2("Sprite/Enemy/RedSlime/PurpleSlimeWalk.png",8));
         enemySpriteAtlasDatabase.put(5, getEnemySpriteAtlas_2("Sprite/Enemy/RedSlime/YellowSlimeWalk.png",8));
 
-        allySpriteAtlasDatabase.put(1, getEnemySpriteAtlas());
-        allySpriteAtlasDatabase.put(2, getEnemySpriteAtlas());
-        allySpriteAtlasDatabase.put(3, getEnemySpriteAtlas());
-        allySpriteAtlasDatabase.put(4, getEnemySpriteAtlas());
-        allySpriteAtlasDatabase.put(5, getEnemySpriteAtlas());
+//        allySpriteAtlasDatabase.put(1, getEnemySpriteAtlas());
+//        allySpriteAtlasDatabase.put(2, getEnemySpriteAtlas());
+//        allySpriteAtlasDatabase.put(3, getEnemySpriteAtlas());
+//        allySpriteAtlasDatabase.put(4, getEnemySpriteAtlas());
+//        allySpriteAtlasDatabase.put(5, getEnemySpriteAtlas());
 
         enemyDatabase.put(1, getEnemyInfoFromTxtFile("src/main/resources/EnemiesInfo/enemy_1.txt"));
         enemyDatabase.put(2, getEnemyInfoFromTxtFile("src/main/resources/EnemiesInfo/enemy_2.txt"));
@@ -54,7 +60,68 @@ public class MainDatabase {
         allyDatabase.put(3, getAllyInfoFromTxtFile("src/main/resources/AllyInfo/ally_3.txt"));
         allyDatabase.put(4, getAllyInfoFromTxtFile("src/main/resources/AllyInfo/ally_4.txt"));
         allyDatabase.put(5, getAllyInfoFromTxtFile("src/main/resources/AllyInfo/ally_5.txt"));
+
+//        Path path = Path.of("src/main/resources/AllyInfo/sprites/blue/character_blue_idle_49x72.txt");
+//        String name="UP_1";
+//        allySpriteDetails.put("1" ,SpriteDetails.createSpriteDetails(path, name));
+//        name="UP_2";
+//        allySpriteDetails.put("2" ,SpriteDetails.createSpriteDetails(path, name));
+//        name="DOWN_1";
+//        allySpriteDetails.put("3" ,SpriteDetails.createSpriteDetails(path, name));
+//        name="DOWN_2";
+//        allySpriteDetails.put("4" ,SpriteDetails.createSpriteDetails(path, name));
+//        name="LEFT_1";
+//        allySpriteDetails.put("5" ,SpriteDetails.createSpriteDetails(path, name));
+//        name="LEFT_2";
+//        allySpriteDetails.put("6" ,SpriteDetails.createSpriteDetails(path, name));
+//        name="RIGHT_1";
+//        allySpriteDetails.put("7" ,SpriteDetails.createSpriteDetails(path, name));
+//        name="RIGHT_2";
+//        allySpriteDetails.put("8" ,SpriteDetails.createSpriteDetails(path, name));
+
+//        for (SpriteDetails spriteDetails : allySpriteDetails.values()) {
+//            System.out.println(spriteDetails.toString());
+//        }
+
+//        LinkedHashMap<Directions, LinkedList<String>> allySpriteAtlas = new LinkedHashMap<>();
+//        LinkedList<String> upSprites = new LinkedList<>();
+//        upSprites.add("1");
+//        allySpriteAtlas.put(Directions.UP, upSprites);
+//
+//        LinkedList<String> downSprites = new LinkedList<>();
+//        downSprites.add("2");
+//        allySpriteAtlas.put(Directions.DOWN, downSprites);
+//
+//        LinkedList<String> leftSprites = new LinkedList<>();
+//        leftSprites.add("3");
+//        allySpriteAtlas.put(Directions.LEFT, leftSprites);
+//
+//        LinkedList<String> rightSprites = new LinkedList<>();
+//        rightSprites.add("4");
+//        allySpriteAtlas.put(Directions.RIGHT, rightSprites);
+//
+//        allySpriteAtlasDatabase.put(1, allySpriteAtlas);
+
+//        for ( : allySpriteAtlas.get(1)) {
+//
+//        }
+
+//        allySpriteAtlasDatabase.put(2, getEnemySpriteAtlas());
+
     }
+
+    public static BufferedImage getSubImageFromSpriteAtlas(BufferedImage spriteAtlas, int xPosAtlas, int yPosAtlas, int subImageWidth, int subImageHeight){
+        return spriteAtlas.getSubimage(xPosAtlas, yPosAtlas, subImageWidth, subImageHeight);
+    }
+
+    public static BufferedImage getSubImageFromSpriteAtlasWithXYCordMultiplier(BufferedImage spriteAtlas,int multiplier, int xPosAtlas, int yPosAtlas, int subImageWidth, int subImageHeight){
+        return spriteAtlas.getSubimage(xPosAtlas*multiplier, yPosAtlas*multiplier, subImageWidth, subImageHeight);
+    }
+
+    public static BufferedImage getSubImageFromSpriteAtlasWithXYDifferentCordMultiplier(BufferedImage spriteAtlas,int multiplierXPos, int multiplierYPos, int xPosAtlas, int yPosAtlas, int subImageWidth, int subImageHeight){
+        return spriteAtlas.getSubimage(xPosAtlas*multiplierXPos, yPosAtlas*multiplierYPos, subImageWidth, subImageHeight);
+    }
+
 
     public static MainDatabase getMainDatabase() {
         if (mainDatabase==null){
@@ -200,7 +267,6 @@ public class MainDatabase {
         for (int height = 0; height < 8; height++) {
             for (int width = 0; width < 8; width++) {
                 Tile tile=new Tile("GRASS_TILE_"+height+"_"+width, 32, 32, ++index, setTile(width, height, 32, 32, spriteAtlasDatabase.get("GrassTileSet")));
-                System.out.println(tile.getId());
                 grassTileSet.put(tile.getId(), tile);
             }
         }
@@ -211,5 +277,7 @@ public class MainDatabase {
         return spriteAtlas.getSubimage(xCord*widthTile, yCord*heightTile, widthTile, heightTile);
     }
 
-
+    public LinkedHashMap<String, BufferedImage> getSpriteAtlasDatabase() {
+        return spriteAtlasDatabase;
+    }
 }
